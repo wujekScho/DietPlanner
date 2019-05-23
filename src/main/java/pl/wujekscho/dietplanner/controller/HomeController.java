d46f17e8d432ba1e837c90647766ac5baa9d1db9
wujekScho <schodziak@gmail.com>
2019-05-23 14:05:52 +0200

    dietplanner - home controller (jenkins test)

--- src/main/java/pl/wujekscho/dietplanner/controller/HomeController.java
+++ src/main/java/pl/wujekscho/dietplanner/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("")
     public String home() {
-        return "hello";
+        return "hello1";
     }
 }
