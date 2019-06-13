c6ff86ad01b60574cab119c0c1061eafaa67e063
wujekScho <schodziak@gmail.com>
2019-06-13 20:09:21 +0200

    changed cors mapping to environment variable

--- src/main/java/pl/wujekscho/dietplanner/config/WebConfig.java
+++ src/main/java/pl/wujekscho/dietplanner/config/WebConfig.java
@@ -1,4 +1,19 @@
 package pl.wujekscho.dietplanner.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(System.getenv("API_HOST"))
+                .allowedHeaders("*");
+    }
 }
