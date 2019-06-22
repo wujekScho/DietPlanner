eb4a9bbbca5f59459a5a402709fa294efd94e6c7
wujekScho <schodziak@gmail.com>
2019-06-22 19:19:20 +0200

    planned day delete and get by id features (controller, service, repo)

--- src/test/java/pl/wujekscho/dietplanner/DietplannerApplicationTests.java
+++ src/test/java/pl/wujekscho/dietplanner/DietplannerApplicationTests.java
@@ -1,16 +1,12 @@
 package pl.wujekscho.dietplanner;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class DietplannerApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 
 }
